eaf0f9e0b408bda90604f8af65ffd304daa63ff6
Hung Nguyen <hung.nguyen@sentifi.com>
2017-07-27 16:06:40 +0700

    integrate api

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '8022563e2a9c56e7e4ffb23f2b1e00f0';
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export default function fetchWeather(city) {
+  const url = `${ROO_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
